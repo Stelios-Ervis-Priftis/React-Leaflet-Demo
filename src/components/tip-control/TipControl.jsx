39bb9aea3dd500a7f872b65d253e41f61cc343fa
Stelios E Priftis <stelios.priftis@sinergise.com>
2023-07-07 13:42:39 +0200

    Clean up tip control component
    Delete unusable variables

--- src/components/tip-control/TipControl.jsx
+++ src/components/tip-control/TipControl.jsx
@@ -3,17 +3,14 @@ import { useState } from 'react';
 import { RiCloseCircleLine } from 'react-icons/all';
 import './tipcontol.scss';
 
-export default function TipControl(props) {
-  const { scrollWheelZoom } = props;
-  const [isPanelClose, setIsPanelClose] = useState(false);
-
+export default function TipControl() {
   const handleIsPanelClose = () => {
     console.log('clicked');
     setIsPanelClose(true);
   };
 
   return (
-    <div className={`tip-control ${isPanelClose || scrollWheelZoom ? 'hide' : ''}`}>
+    <div className={'tip-control'}>
       <p>
         To activate <span>Scroll Wheel Zoom</span> functionality press and hold <span>W</span> Key on your
         Keyboard
