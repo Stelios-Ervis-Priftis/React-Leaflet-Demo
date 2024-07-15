b4034e99e043911f22a39d05cc8329019d36b4b9
Stelios Priftis <steliospriftis@gmail.com>
2024-07-15 11:57:33 +0200

    Start developing an action control system

--- src/components/reset-map-control/ResetMapControl.jsx
+++ src/components/reset-map-control/ResetMapControl.jsx
@@ -1,13 +1,25 @@
 import { TbZoomOutArea } from 'react-icons/all';
 import './resetMapControl.scss';
 
+const tools = [
+  {
+    name: 'magnifier-reset-control',
+    text: 'Tip: Click the icon to Reset the map and see all countries again',
+    icon: <TbZoomOutArea className="reset-icon" />,
+  },
+];
+
 export default function ResetMapControl({ handleMapReset }) {
-  return (
-    <div className={`reset-tip-control`}>
-      <p>
-        <span>Tip:</span> Click the icon to Reset the map and see all countries again
-      </p>
-      <TbZoomOutArea className="reset-icon" onClick={handleMapReset} />
-    </div>
-  );
+  return tools.map((tool, i) => {
+    console.log(Object.keys(tools[i]));
+    return (
+      <div key={i}>
+        <div className={tool.name}>
+          <div>{tool.text}</div>
+
+          <div>{tool.icon}</div>
+        </div>
+      </div>
+    );
+  });
 }
